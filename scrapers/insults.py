e3cd7c0524dcf20d4e2e1aacd2b3195c36b7fd7d
SB <19213803+Jester-Head@users.noreply.github.com>
2022-11-27 23:54:38 -0500

    working

---
+++ scrapers/insults.py
@@ -0,0 +1,147 @@
+import os
+from time import sleep
+from random import randint
+
+import pandas as pd
+import undetected_chromedriver.v2 as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+def create_chrome_driver():
+    options = uc.ChromeOptions()
+
+    # setting profile
+    options.add_argument('--user-data-dir=c:\\temp\\profile')
+    
+    # Change browser options
+    options.add_argument('proxy-server = 106.122.8.54:3128')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249 Safari/537.36')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument('--start-maximized')
+    
+    # options passing in to skip popups
+    options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+    driver = uc.Chrome(options=options)
+    
+    return driver
+
+
+
+
+def file_exists(filename,headers):
+    filename = 'data\\'+ filename +'.csv'
+    # Create file if doesn't exist
+    if not os.path.exists(filename):
+        with open(filename, 'w',encoding='utf-8') as f:
+            f.writelines(headers)
+    # Add headers if file exists without headers
+    else:
+        with open (filename,'r+',encoding='utf-8') as f:
+            if str(f.readline())=='':
+                f.writelines(headers)
+    return filename
+    
+    
+def extract_insult_names(driver,url,insult_type,selector,button,count):
+    insult = []
+    with driver:
+        driver.get(url)
+        sleep(randint(1,5))
+        for i in range(count):
+            driver.find_element(By.CSS_SELECTOR,button).click()
+            sleep(2)
+            # Checks if page updated and waits to append if not
+            if((i-1) == (driver.find_element(By.CSS_SELECTOR,selector).text)):
+                sleep(5)
+                insult.append(driver.find_element(By.CSS_SELECTOR,selector).text)
+            else:
+                insult.append(driver.find_element(By.CSS_SELECTOR,selector).text)
+    df = pd.DataFrame(insult,columns=['insult'])
+    df['type'] = insult_type
+    df['link'] = url
+    return df
+
+
+def save_data(data,filename):
+    data.to_csv(filename,mode='a',index=False,header=False,encoding='utf-8')
+
+def clean_data(df):
+    df.drop_duplicates(inplace=True)
+    df['insult'] = [i.lower() for i in df['insult']]
+    df['insult']=df['insult'].apply(lambda x: x.replace('0','o'))
+    df['insult']=df['insult'].apply(lambda x: x.replace('5','s'))
+    df['insult']=df['insult'].apply(lambda x: x.replace('1','i'))
+    df['insult']=df['insult'].apply(lambda x: x.replace('3','e'))
+    df['insult']=df['insult']=df['insult'].apply(lambda x: x.replace('**','uc'))
+    df['insult']=df['insult']=df['insult'].apply(lambda x: x.replace('*','i'))
+    df['insult']=df['insult']=df['insult'].apply(lambda x: x.replace('slvt','slut'))
+    df['insult']=df['insult']=df['insult'].apply(lambda x: x.replace('!',''))
+    df['insult']=df['insult']=df['insult'].apply(lambda x: x.replace('you',''))
+    df['insult']=df['insult']=df['insult'].apply(lambda x: x.replace('thou',''))
+    df['insult']=df['insult'].str.strip()
+    return df
+    
+        
+def main():
+
+    driver = create_chrome_driver()
+    headers = ['insult,','category,','link\n']
+    file = file_exists('insult_names',headers)
+    
+    robie_robot_names = 'http://robietherobot.com/insult-generator.htm'
+    button = 'tr:nth-child(1) td:nth-child(2) form:nth-child(4) center:nth-child(6) > input:nth-child(1)'
+    selector = 'center td h1'
+    insult_type = 'nsfw'
+    count = 100
+    df1 = extract_insult_names(driver=driver,url=robie_robot_names,insult_type=insult_type,selector=selector,button=button,count=count)
+    
+    rust_names = 'https://rusttips.com/insult-generator/'
+    selector ='.wpb_content_element .wpb_wrapper h2'
+    button = '.bklyn-btn-normal'
+    insult_type = 'nsfw'
+    count = 300
+    df2 = extract_insult_names(driver=driver,url=rust_names,insult_type=insult_type,selector=selector,button=button,count=count)
+    
+    kids = 'https://fungenerators.com/random/insult/childish/'
+    button = '.btn-home'
+    selector='h2'
+    insult_type = 'kid_friendly'
+    count = 100
+    
+    df3 = extract_insult_names(driver=driver,url=kids,insult_type=insult_type,selector=selector,button=button,count=count)
+    
+        
+    shakespeare = 'https://fungenerators.com/random/insult/shakespeare/'
+    button = '.btn-home'
+    selector='h2'
+    insult_type = 'shakespeare'
+    count = 50
+    df4 = extract_insult_names(driver=driver,url=shakespeare,insult_type=insult_type,selector=selector,button=button,count=count)
+    
+    medieval_insult='https://fungenerators.com/random/insult/medieval-insult/'
+    button = '.btn-home'
+    selector='h2'
+    insult_type = 'medieval'
+    count = 50
+    
+    df5 = extract_insult_names(driver=driver,url=medieval_insult,insult_type=insult_type,selector=selector,button=button,count=count)
+    
+    
+    df = pd.concat([df1,df2,df3,df4,df5], ignore_index=True, sort=False)
+    df = clean_data(df)
+    save_data(data = df,filename=file)
+    driver.quit()
+    
+
+if __name__ == "__main__":
+    main()
+    
---
+++ scrapers/no_detect.py
@@ -0,0 +1,117 @@
+# __author__ = "Soumil Shah"
+# __email__ = "shahsoumil519@gmail.com"
+try:
+
+    import os
+    import sys
+    import time
+
+    from bs4 import BeautifulSoup
+    from fake_useragent import UserAgent
+    from fp.fp import FreeProxy
+    from selenium import webdriver
+    from selenium.common.exceptions import TimeoutException
+    from selenium.webdriver import Chrome
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.action_chains import ActionChains
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.chrome.service import Service
+
+    print('all module are loaded ')
+
+except Exception as e:
+
+    print("Error ->>>: {} ".format(e))
+
+
+class Spoofer(object):
+
+    def __init__(self, country_id=['US'], rand=True, anonym=True):
+        self.country_id = country_id
+        self.rand = rand
+        self.anonym = anonym
+        self.userAgent, self.ip = self.get()
+
+    def get(self):
+        ua = UserAgent()
+        proxy = FreeProxy(country_id=self.country_id, rand=self.rand, anonym=self.anonym).get()
+        ip = proxy.split("://")[1]
+        return ua.random, ip
+
+
+class DriverOptions(object):
+
+    def __init__(self):
+
+        self.options = Options()
+        self.options.add_argument('--no-sandbox')
+        self.options.add_argument('--start-maximized')
+        self.options.add_argument('--start-fullscreen')
+        self.options.add_argument('--single-process')
+        self.options.add_argument('--disable-dev-shm-usage')
+        self.options.add_argument("--incognito")
+        self.options.add_argument('--disable-blink-features=AutomationControlled')
+        self.options.add_argument('--disable-blink-features=AutomationControlled')
+        self.options.add_experimental_option('useAutomationExtension', False)
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.options.add_argument("disable-infobars")
+
+        self.helperSpoofer = Spoofer()
+
+        self.options.add_argument('user-agent={}'.format(self.helperSpoofer.userAgent))
+        self.options.add_argument('--proxy-server=%s' % self.helperSpoofer.ip)
+
+
+class WebDriver(DriverOptions):
+
+    def __init__(self, path=''):
+        DriverOptions.__init__(self)
+        self.driver_instance = self.get_driver()
+
+    def get_driver(self):
+
+        print("""
+        IP:{}
+        UserAgent: {}
+        """.format(self.helperSpoofer.ip, self.helperSpoofer.userAgent))
+
+        PROXY = self.helperSpoofer.ip
+        webdriver.DesiredCapabilities.CHROME['proxy'] = {
+            "httpProxy":PROXY,
+            "ftpProxy":PROXY,
+            "sslProxy":PROXY,
+            "noProxy":None,
+            "proxyType":"MANUAL",
+            "autodetect":False
+        }
+        webdriver.DesiredCapabilities.CHROME['acceptSslCerts'] = True
+
+        # path = os.path.join(os.getcwd(), '../windowsDriver/chromedriver.exe')
+
+        # driver = webdriver.Chrome(executable_path=path, options=self.options)
+        service = Service('C:\chromedriver_win32\chromedriver')
+        driver = webdriver.Chrome(service=service)
+        driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source":
+                "const newProto = navigator.__proto__;"
+                "delete newProto.webdriver;"
+                "navigator.__proto__ = newProto;"
+        })
+
+        return driver
+
+
+def main():
+
+    driver= WebDriver()
+    driverinstance = driver.driver_instance
+    driverinstance.get("https://www.google.com/")
+    time.sleep(5)
+    print("done")
+
+if __name__ == "__main__":
+    main()
